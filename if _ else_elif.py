becca8a617f096151310e891415878bc18cd97e7
Devanshu <kdevanshu534@gmail.com>
2022-08-26 00:50:20 +0530

    Code

---
+++ if _ else_elif.py
@@ -0,0 +1,13 @@
+
+#2 Take three values from user and find the greatest number from them.
+x = int(input(" Enter value : "))
+y = int(input(" Enter value : "))
+z = int(input(" Enter value : "))
+
+if x > y and x < z:
+    largest = x
+elif y > x and y > z:
+    largest = y
+else:
+    largest = z
+print(" largest number is", largest)
